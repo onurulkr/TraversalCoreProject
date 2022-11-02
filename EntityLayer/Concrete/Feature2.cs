bd0825056436db9528985b0365b7f625113ff4a2
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-11-02 14:40:14 +0300

    add layers, class, interfaces

--- EntityLayer/Concrete/Feature2.cs
+++ EntityLayer/Concrete/Feature2.cs
@@ -7,16 +7,13 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class Destination
+    public class Feature2
     {
         [Key]
-        public int DestinationId { get; set; }
-        public string City { get; set; }
-        public string DayNight { get; set; }
-        public double Price { get; set; }
+        public int Feature2Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
         public string Image { get; set; }
-        public bool Description { get; set; }
-        public int Capacity { get; set; }
         public bool Status { get; set; }
     }
 }
--- EntityLayer/Concrete/Guide.cs
+++ EntityLayer/Concrete/Guide.cs
@@ -7,16 +7,15 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class Destination
+    public class Guide
     {
         [Key]
-        public int DestinationId { get; set; }
-        public string City { get; set; }
-        public string DayNight { get; set; }
-        public double Price { get; set; }
+        public int GuidId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public string Image { get; set; }
-        public bool Description { get; set; }
-        public int Capacity { get; set; }
+        public string TwitterUrl { get; set; }
+        public string InstagramUrl { get; set; }
         public bool Status { get; set; }
     }
 }
