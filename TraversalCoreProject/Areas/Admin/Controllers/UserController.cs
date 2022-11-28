3afefdb074d93f8754e57a86af46848069362bc8
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-11-28 17:44:12 +0300

    error pages, guide list, serilog, guide operations

--- TraversalCoreProject/Areas/Admin/Controllers/UserController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/UserController.cs
@@ -12,10 +12,12 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly IAppUserService _appUserService;
+        private readonly IReservationService _reservationService;
 
-        public UserController(IAppUserService appUserService)
+        public UserController(IAppUserService appUserService, IReservationService reservationService)
         {
             _appUserService = appUserService;
+            _reservationService = reservationService;
         }
 
         public IActionResult Index()
@@ -58,9 +60,9 @@ namespace TraversalCoreProject.Areas.Admin.Controllers
 
         public IActionResult ReservationUser(int id)
         {
-            _appUserService.TGetList();
+            var values = _reservationService.GetListWithReservationByAccepted(id);
 
-            return View();
+            return View(values);
         }
     }
 }
