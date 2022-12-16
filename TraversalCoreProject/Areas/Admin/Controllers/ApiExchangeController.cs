e800f9d7a115dafd4a413bc52f44bec96b128e7c
onurulkr <55154950+onurulkr@users.noreply.github.com>
2022-12-16 15:16:27 +0300

    rapid api booking exchange api, imdb film api

---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    public class ApiExchangeController : Controller
+    {
+		[Area("Admin")]
+		[AllowAnonymous]
+		public async Task<IActionResult> Index()
+        {
+			List<BookingExchangeViewModel2> bookingExchangeViewModels = new List<BookingExchangeViewModel2>();
+
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "13e0f27fefmshcc18f89a7bca370p1a7e8ejsn4b7011472cef" },
+		{ "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+
+				return View(values.exchange_rates);
+			}
+		}
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "13e0f27fefmshcc18f89a7bca370p1a7e8ejsn4b7011472cef" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+
+                return View(apiMovies);
+            }
+        }
+    }
+}
