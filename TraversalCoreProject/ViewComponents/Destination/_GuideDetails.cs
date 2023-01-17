57ff369896c9e7b20c75eec589ca85531f640a2c
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-01-17 15:29:35 +0300

    design arrangements 2

---
+++ TraversalCoreProject/ViewComponents/Destination/_GuideDetails.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.Destination
+{
+    public class _GuideDetails : ViewComponent
+    {
+        private readonly IGuideService _guideService;
+
+        public _GuideDetails(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _guideService.TGetById(1);
+
+            return View(values);
+        }
+    }
+}
