5463bdbb2b464f0ca9e6ef9083f2fa2ad2b27515
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-04-17 13:27:57 +0300

    Update CreateDestinationCommand.cs

--- TraversalCoreProject/CQRS/Commands/DestinationCommands/CreateDestinationCommand.cs
+++ TraversalCoreProject/CQRS/Commands/DestinationCommands/CreateDestinationCommand.cs
@@ -10,6 +10,6 @@ namespace TraversalCoreProject.CQRS.Commands.DestinationCommands
         public string City { get; set; }
         public string DayNight { get; set; }
         public double Price { get; set; }
-        public int Capacity { get; set; }
+        public int Capacity { get; set; } 
     }
 }
