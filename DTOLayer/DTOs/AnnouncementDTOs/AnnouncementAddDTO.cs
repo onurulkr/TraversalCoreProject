3698d419fb002fde8f651f59fd91b678c5a609e3
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-11-22 14:52:05 +0300

    Update AnnouncementAddDTO.cs

--- DTOLayer/DTOs/AnnouncementDTOs/AnnouncementAddDTO.cs
+++ DTOLayer/DTOs/AnnouncementDTOs/AnnouncementAddDTO.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace DTOLayer.DTOs.AnnouncementDTOs
 {
     public class AnnouncementAddDTO 
-    {
+    { 
         public string Title { get; set; }
         public string Content { get; set; }
     }
